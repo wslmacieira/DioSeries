0615cd6997a151dec73274cc6e0189069802f9d4
wslmacieiradev <wslmacieira@outlook.com>
2021-03-04 10:04:36 -0300

    Criando Projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dio.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

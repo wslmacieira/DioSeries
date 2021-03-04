aca085bbd6f36056a6350888eea08ae2c02abc07
wslmacieiradev <wslmacieira@outlook.com>
2021-03-04 11:56:36 -0300

    Implementando ListarSeries e InserirSerie

--- Program.cs
+++ Program.cs
@@ -1,15 +1,115 @@
 ﻿using System;
+using Dio.Series.Classes;
 
 namespace Dio.Series
 {
     class Program
     {
+        static SerieRepositorio repositorio = new SerieRepositorio();
         static void Main(string[] args)
         {
+            string opcaoUsuario = ObterOpcaoUsuario();
 
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarSeries();
+                        break;
+                    case "2":
+                        InserirSerie();
+                        break;
+                    case "3":
+                        //AtualizarSerie();
+                        break;
+                    case "4":
+                        //ExcluirSerie();
+                        break;
+                    case "5":
+                        //VisualizarSerie();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
 
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static void InserirSerie()
+        {
+
+            Console.WriteLine("Inserir nova série");
+
+            //https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getvalues?view=net-5.0
+            //https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getname?view=net-5.0
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+
+            Console.Write("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Título da série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Ano da série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Descrição da série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie novaSerie = new Serie(id: repositorio.ProximoId(),
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        descricao: entradaDescricao,
+                                        ano: entradaAno);
+
+            repositorio.Insere(novaSerie);
+        }
+
+        private static void ListarSeries()
+        {
+            Console.WriteLine("Listar séries");
+
+            var lista = repositorio.Lista();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série cadastrada.");
+                return;
+            }
+
+            foreach (var serie in lista)
+            {
+                Console.WriteLine("#ID {0}: - {1}", serie.RetornaId(), serie.RetornaTitulo());
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("DIO Séries a seu dispor!!!");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1- Listar séries");
+            Console.WriteLine("2- Inserir nova série");
+            Console.WriteLine("3- Atualizar série");
+            Console.WriteLine("4- Excluir série");
+            Console.WriteLine("5- Visualizar série");
+            Console.WriteLine("C- Limpar Tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
